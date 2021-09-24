81b216be569d3f0497078cc902279ca832992e7f
Pedrohero13 <pedrogadote@gmail.com>
2021-09-24 15:25:01 -0500

    services #6

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {trigger,style,transition,animate,keyframes,query,stagger} from '@angular/animations';
+import { DataService } from '../data.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -15,12 +16,12 @@ import {trigger,style,transition,animate,keyframes,query,stagger} from '@angular
             style({opacity: 0.5, transform:'translateY(35px)', offset: .3}),
             style({opacity: 1, transform:'translateY(0)', offset: 1}),
           ]))]),{optional: true}),
-          query(':leave', stagger('300ms', [
-            animate('.6s ease-in', keyframes([
-              style({opacity: 1, transform:'translateY(0)', offset: 0}),
-              style({opacity: 0.5, transform:'translateY(35px)', offset: .3}),
-              style({opacity: 0, transform:'translateY(75%)', offset: 1}),
-            ]))]),{optional: true})
+        query(':leave', stagger('300ms', [
+          animate('.6s ease-in', keyframes([
+            style({opacity: 1, transform:'translateY(0)', offset: 0}),
+            style({opacity: 0.5, transform:'translateY(35px)', offset: .3}),
+            style({opacity: 0, transform:'translateY(-75%)', offset: 1}),
+          ]))]),{optional: true})
         ])
       ])
   ] 
@@ -32,21 +33,26 @@ export class HomeComponent implements OnInit {
   goalText: string = "My first goal"
   goals: string  []= [];
 
-  constructor() { }
+  constructor(private _data:DataService) { }
 
   ngOnInit(): void {
+    
+    this._data.goal.subscribe(res => this.goals = res);
     this.itemCount = this.goals.length;
+    this._data.changeGoal(this.goals)
   }
 
   addItem(){
     this.goals.push(this.goalText);
     this.goalText="";
     this.itemCount= this.goals.length;
+    this._data.changeGoal(this.goals)
 
   }
   removeItem(i: number){
     this.goals.splice(i,1);
     this.itemCount = this.goals.length;
+    this._data.changeGoal(this.goals)
  }
 
 }
